13deb80d59874f754f831c99c615afe16570c9af
indrabijaynarayan <root@MANISHAs-MBP.Dlink>
2020-04-22 16:42:56 +0530

    created a seperate Course list page

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,49 +1,24 @@
-import React from 'react';
+import React , { useState , useEffect }from 'react';
 import { getCourses } from '../api/courseApi';
+import CourseList from './CourseList';
 
-class CoursesPage extends React.Component{
-    state={ courses:[]};
+function CoursesPage (){
+    //initialize the state to empty value.
+    const [courses, setCourses]=useState([]);
 
-    componentDidMount(){
-        getCourses().then((courses)=>{
-            this.setState({courses: courses});
+    useEffect(()=>{
+        getCourses().then((_courses)=> {
+            setCourses(_courses);
         });
-        
-    }
+    },[]);
     
-    render(){
         return(
             <>
         <h2>Courses</h2>
-        <table className="table">
-            <thead>
-            <tr>
-                <th>
-                    Title
-                </th>
-                <th>
-                    Author Id
-                </th>
-                <th>
-                    Category
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-                {this.state.courses.map(course =>{
-                    return (
-                    <tr key={course.id}>
-                        <td>{course.title}</td>
-                        <td>{course.authorId}</td>
-                        <td>{course.category}</td>
-                    </tr>
-                );
-                })};    
-            </tbody>
-        </table>
+        <CourseList courses={courses}/>
         </>
         )
-    }
+    
 }
 
 export default CoursesPage;
